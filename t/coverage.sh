9e413c88e808c29de22f66d2a4c5d262358e7e91
David Cantrell <david@cantrell.org.uk>
2016-10-28 22:44:16 +0100

    turn AUTHOR_TESTING on for coverage

--- t/coverage.sh
+++ t/coverage.sh
@@ -2,5 +2,5 @@
 # $Id: coverage.sh,v 1.1 2008/02/26 21:54:55 drhyde Exp $
 
 cover -delete
-HARNESS_PERL_SWITCHES=-MDevel::Cover make test
+AUTHOR_TESTING=1 HARNESS_PERL_SWITCHES=-MDevel::Cover make test
 cover
